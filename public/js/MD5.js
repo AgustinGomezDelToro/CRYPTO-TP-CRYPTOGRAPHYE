bf662c1de6b5b98ec09e903b2e90832973cdb209
AgustinGomezDelToro <66341782+AgustinGomezDelToro@users.noreply.github.com>
2023-02-09 13:22:37 +0100

    Fontions for differents hashages

---
+++ public/js/MD5.js
@@ -0,0 +1,58 @@
+///HASHAGE MD5
+
+
+$(document).ready(function(){
+	//		
+	
+		$("#btnEncripta").on("click",function(){
+			$.ajax({type: "GET",
+				   url: "encriptar/encrip",
+				   dataType: "json",
+				   data: "frase="+$("#frase").val()+"&pass="+$("#pass").val(),
+				   success: fnCallBack,
+				   error:fnErrorCallBack
+			});
+		});
+	
+		$("#btnDecifra").on("click",function(){
+			$.ajax({type: "GET",
+				   url: "encriptar/decrip",
+				   dataType: "json",
+				   data: "resultado="+$("#resultado").text()+"&pass="+$("#pass").val(),
+				   success: fnDecifraCallBack,
+				   error:fnErrorCallBack
+			});
+		});
+	
+	 });
+	
+	
+	function fnErrorCallBack(data){
+		console.log(data.responseText);
+	
+		$("#resultadoOriginal").text(data.responseText);
+	
+	
+	}
+	
+	function fnDecifraCallBack(data){
+	
+		$("#resultadoOriginal").text(data.respuesta);
+	}
+	
+	function fnCallBack(data){
+	
+		$("#resultado").text(data.respuesta);
+	}
+	
+	function calculateHash() {
+		var input = document.getElementById("fileToUpload").files[0];
+		var reader = new FileReader();
+		reader.onload = function(event) {
+		  var data = event.target.result;
+            // hash MD5
+            var hash = CryptoJS.MD5("Message");
+		  document.getElementById("resultado2").innerHTML = "<p>" + hash.toString('') + "</p>";
+		};
+		reader.readAsBinaryString(input);
+	  }
